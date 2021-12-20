23d4df6bbe95b97332ea6b0aa1eae616ebc3d49b
Vitalii <vitaliikalyta96@gmail.com>
2021-12-21 00:19:39 +0200

    Dropped the path to render file html .

--- src/api/routes/main.py
+++ src/api/routes/main.py
@@ -4,31 +4,34 @@ from utils.helpers import convert_list
 # from models import Event
 from models.models import Event
 
+
 @app.route('/')
 def main():
     events = Event.query.all()
-    return render_template('index.html', events=events)
+    return render_template('', events=events)
 
 
 @app.route('/event/<int:id>')
 def event(id):
     event = Event.query.get(id)
-    return render_template('edit-event.html', event=event)
+    return render_template('', event=event)
 
 
 @app.route('/event/<int:id>/edit')
 def event_edit_page(id):
     event = Event.query.get(id)
-    return render_template('edit-event.html', event=event)
+    return render_template('', event=event)
 
 
 @app.route('/event/<int:id>/update', methods=['POST'])
 def event_update(id):
     event = Event.query.get(id)
     form_data = request.form
-    event.name_mentor = form_data.get('name_mentor')
-    event.date = form_data.get('date')
-    event.time = form_data.get('time')
+    event.title = form_data.get('title')
+    event.description = form_data.get('description')
+    event.date_time = form_data.get('date_time')
+    event.category = form_data.get('category')
+    event.link = form_data.get('link')
     db.session.add(event)
     db.session.commit()
     return redirect(url_for('event', id=id))
