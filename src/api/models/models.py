a7c929c634f278df01e242bee22d28f5825837b2
Vitalii <vitaliikalyta96@gmail.com>
2021-12-22 00:00:18 +0200

    Created 2 models events and category.

--- src/api/models/models.py
+++ src/api/models/models.py
@@ -54,3 +54,75 @@ class User(db.Model):
             "email": self.email
 
         }
+
+
+class Event(db.Model):
+    __tablename__ = "events"
+    __table_args__ = {'extend_existing': True}
+    id = db.Column(
+        db.Integer,
+        primary_key=True
+    )
+
+    title = db.Column(
+        db.String(30),
+        nullable=False
+    )
+
+    description = db.Column(
+        db.String(100),
+        nullable=False
+    )
+
+    date_time = db.Column(
+        db.String(30),
+        nullable=False
+    )
+
+    category = db.Column(
+        db.String(100),
+        nullable=False
+    )
+
+    link = db.Column(
+        db.String(100),
+        nullable=False
+    )
+
+    @property
+    def serialize(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'description': self.description,
+            'date_time': self.date_time,
+            'category': self.category,
+            'link': self.link,
+        }
+
+
+class Category(db.Model):
+    __tablename__ = "category"
+    __table_args__ = {'extend_existing': True}
+    id = db.Column(
+        db.Integer,
+        primary_key=True
+    )
+
+    title = db.Column(
+        db.String(30),
+        nullable=False
+    )
+
+    description = db.Column(
+        db.String(100),
+        nullable=False
+    )
+
+    @property
+    def serialize(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'description': self.description,
+        }
