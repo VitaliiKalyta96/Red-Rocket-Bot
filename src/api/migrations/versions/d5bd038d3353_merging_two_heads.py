b3c3bd34b0e1310715f1faba87032c1e73aa9faf
Vitalii <vitaliikalyta96@gmail.com>
2021-12-21 23:54:27 +0200

    Fixed Error: Multiple heads.

---
+++ src/api/migrations/versions/d5bd038d3353_merging_two_heads.py
@@ -0,0 +1,24 @@
+"""merging two heads
+
+Revision ID: d5bd038d3353
+Revises: 40c4309334ed, 3790cc196655
+Create Date: 2021-12-21 21:38:54.574352
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'd5bd038d3353'
+down_revision = ('40c4309334ed', '3790cc196655')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
